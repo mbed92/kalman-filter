71c3771ed68afa972400266889d69344d734efdc
mbed92 <michalbednarek.e@gmail.com>
2020-12-10 14:06:53 +0100

    initial values updates

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 '''
-The example of a localization using Kalman Filter.
+A very simple example of a localization using Kalman Filter.
 Author: Michał Bednarek
 '''
 
@@ -7,12 +7,12 @@ import numpy as np
 
 import kf
 
-# initialize the kalman filter
+# initialize the Kalman filter
 algo = kf.algorithm.base_kalman.KalmanFilter(ndim=2)
 state = np.asarray([[0., 0.], [0., 0.]])            # position and velocity of a moving point
-uncertainty = np.eye(state.ndim) * 10.              # initial uncertainty
-x = np.linspace(0, 10, 10)                          # path (measurements)
-y = np.sin(x)
+uncertainty = np.eye(state.ndim) * 15.              # initial uncertainty
+x = np.linspace(0, 14, 30)                          # path (measurements)
+y = np.cos(0.5 * x)
 measurements = np.stack([x, y]).T
 env = kf.world.EmptyWorld2D(15, 5)                  # initialize the world
 
