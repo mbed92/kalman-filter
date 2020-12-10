3e6104a54a9b850085f073fc65f56654637e5730
mbed92 <michalbednarek.e@gmail.com>
2020-12-10 14:47:55 +0100

    gaussian rotated wrt toe velocity

--- localization_2D.py
+++ localization_2D.py
@@ -12,7 +12,7 @@ algo = kf.algorithm.base_kalman.KalmanFilter(ndim=2)
 state = np.asarray([[0., 0.], [0., 0.]])            # position and velocity of a moving point
 uncertainty = np.eye(state.ndim) * 15.              # initial uncertainty
 x = np.linspace(0, 14, 100)                        # path (noisy measurements)
-y = np.cos(2 * x) + np.random.randn()
+y = np.cos(x) + 0.3 * np.random.randn(x.shape[0])
 measurements = np.stack([x, y]).T
 env = kf.world.EmptyWorld2D(15, 5)                  # initialize the world
 
