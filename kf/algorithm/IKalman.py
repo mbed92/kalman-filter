03c6a716937d3e71ef06d0dd941507dffe21726f
mbed92 <michalbednarek.e@gmail.com>
2020-12-10 14:26:09 +0100

    updated external motion mat

--- kf/algorithm/IKalman.py
+++ kf/algorithm/IKalman.py
@@ -4,7 +4,7 @@ import numpy as np
 class IKalman(object):
     def __init__(self, ndim: int):
         self.ndim = ndim
-        self.external_motion_vector = np.array([[0.], [0.]])
+        self.external_motion_vector = np.array([[0., 0.], [0, 0.]])
         self.next_state_matrix = np.array([[1., 1.], [0, 1.]])
         self.state_matrix = np.array([[1., 0.]])
         self.num_observables = self.state_matrix.sum().astype(int)
